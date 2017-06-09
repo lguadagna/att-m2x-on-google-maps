8ef208acc31363a56ce45af0b1e2e25562a6d99d
Lisa <lisag@jnlcorp.com>
2017-06-09 08:36:20 -0700

    forgot locations.js

---
+++ js/locations.js
@@ -0,0 +1,83 @@
+   var locations = [
+
+       {
+           id: '41d853773e740b89da629dbca6570c2a',
+           name: '101 Cooper St',
+           location: {
+               lat: 36.9752703,
+               lng: -122.0283402,
+               region: "SC"
+           },
+           bpsu: 10200,
+           bpsd: 3000
+       },
+       {
+           id: '3be9579d6089fe95dbffb0186f752be4',
+           name: 'Valencia Apartments, San Louis Obispo',
+           location: {
+               lat: 35.29287,
+               lng: -120.6762382,
+               region: "MID"
+           },
+           bpsu: 44000,
+           bpsd: 40000
+       },
+       {
+           id: 'a58829f26dc86cc96c4539240d8676ce',
+           name: 'Swanton Berry Farm',
+           location: {
+               lat: 37.0303727,
+               lng: -122.2205668,
+               region: "SC"
+           },
+           bpsu: 773,
+           bpsd: 45000
+       },
+       {
+           id: 'e065dbf7a12c43f7e0d910ea31992826',
+           name: 'RoadHouse',
+           location: {
+               lat: 37.0109223,
+               lng: -122.1938546,
+               region: "SC"
+           },
+           bpsu: 45000,
+           bpsd: 45000
+       },
+
+       {
+           id: 'a0f2093dd325f1e43a175839d02d049f',
+           name: 'Lassens',
+           location: {
+               lat: 35.2948578,
+               lng: -120.6698484,
+               region: "MID"
+           },
+           bpsu: 45000,
+           bpsd: 45000
+       },
+       {
+           id: 'd188b6e5617cb5b0cfdb9b26217b4257',
+           name: 'scoutCoffeeHouse',
+           location: {
+               lat: 35.2945799846,
+               lng: -120.669866502,
+               region: "MID"
+           },
+           bpsu: 45000,
+           bpsd: 45000
+       },
+       {
+           id: '1bce8e5ed43a5daaf85bc4516f8a575d',
+           name: 'San Jose Home',
+           location: {
+               lat: 37.3941,
+               lng: -121.835,
+               region: "BA"
+           },
+           bpsu: 45000,
+           bpsd: 45000
+       }
+
+
+   ];
